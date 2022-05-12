6c87c7aa5ac67428894edb239a47a38d8f5be55b
nnlynx <gmfnikita@gmail.com>
2022-05-12 16:27:26 +0300

    Отредактированы классы парсеров

--- src/parsing.py
+++ src/parsing.py
@@ -14,85 +14,56 @@ import datetime
 from html.parser import HTMLParser
 
 
-class Main_parser(HTMLParser):
+class BaseDataParser(HTMLParser):
     def __init__(self):
-        HTMLParser.__init__(self)
+        super().__init__()
+        self.wagon_attrs = {}
+        self.row_count = 0
+        self.flag = {
+            "th": False,  # Заголовок таблицы
+            "tbody": False,  # Таблица
+            "tr": False,  # Строка таблицы
+            "td": False  # Ячейка строки таблицы
+        }
+
+    def handle_starttag(self, tag, attrs):
+        self.flag[tag] = True
+
+    def handle_endtag(self, tag):
+        if self.is_row_ended(tag):
+            self.row_count += 1
+        self.flag[tag] = False
+
+    def handle_data(self, data):
+        if self.is_table_row():
+            self.fill_wagon_attrs(data)
+
+    def is_row_ended(self, tag):
+        return tag == "tr" and self.flag["tbody"]
+
+    def is_table_row(self):
+        return self.flag["tbody"] and self.flag["tr"]
+
+    def fill_wagon_attrs(self, data):
+        self.wagon_attrs[self.row_count] = self.wagon_attrs.get(
+            self.row_count, [])
+        if self.flag["td"]:
+            self.wagon_attrs[self.row_count].append(data)
+
+
+class ExtraDataParser(BaseDataParser):
+    def __init__(self):
+        super().__init__()
         self.raw_start_date = None
-        self.wagon_attrs = {}
-        self.row_count = 0
-        self.th_flag = False  # Заголовок таблицы
-        self.tbody_flag = False  # Ячейка строки таблицы
-        self.tr_flag = False  # Строка таблицы
-        self.td_flag = False  # Ячейка строки таблицы
-
-    def handle_starttag(self, tag, attrs):
-        if tag == "tr":
-            self.tr_flag = True
-        elif tag == "td":
-            self.td_flag = True
-        elif tag == "tbody":
-            self.tbody_flag = True
-        elif tag == "th":
-            self.th_flag = True
-
-    def handle_endtag(self, tag):
-        if tag == "tr":
-            self.tr_flag = False
-            if self.tbody_flag:
-                self.row_count += 1
-        elif tag == "td":
-            self.td_flag = False
-        elif tag == "tbody":
-            self.tbody_flag = False
-        elif tag == "th":
-            self.th_flag = False
 
     def handle_data(self, data):
-        if self.tbody_flag:
-            if self.tr_flag:
-                self.wagon_attrs[self.row_count] = self.wagon_attrs.get(
-                    self.row_count, [])
-                if self.td_flag:
-                    self.wagon_attrs[self.row_count].append(data)
-        if self.th_flag:
-            if (self.raw_start_date is None) and (" по " in data):
-                self.raw_start_date = data[-10:]
+        if self.if_header_contains_date(data):
+            self.raw_start_date = data[-10:]
+        if self.is_table_row():
+            self.fill_wagon_attrs(data)
 
-
-class Extra_parser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.wagon_attrs = {}
-        self.row_count = 0
-        self.tbody_flag = False  # Ячейка строки таблицы
-        self.tr_flag = False  # Строка таблицы
-        self.td_flag = False  # Ячейка строки таблицы
-
-    def handle_starttag(self, tag, attrs):
-        if tag == "tr":
-            self.tr_flag = True
-        elif tag == "td":
-            self.td_flag = True
-        elif tag == "tbody":
-            self.tbody_flag = True
-
-    def handle_endtag(self, tag):
-        if tag == "tr":
-            self.tr_flag = False
-            if self.tbody_flag:
-                self.row_count += 1
-        elif tag == "td":
-            self.td_flag = False
-        elif tag == "tbody":
-            self.tbody_flag = False
-
-    def handle_data(self, data):
-        if self.tbody_flag:
-            if self.tr_flag:
-                self.wagon_attrs[self.row_count] = self.wagon_attrs.get(
-                    self.row_count, [])
-                if self.td_flag:
-                    self.wagon_attrs[self.row_count].append(data)
+    def if_header_contains_date(self, data):
+        return self.flag["th"] and (self.raw_start_date is None) and (" по " in data)
 
 
 def wagon_data_main_parser(pars_path):
@@ -102,7 +73,7 @@ def wagon_data_main_parser(pars_path):
     со списками необработанных аргументов для экземпляров класса Wagon()
     """
     with open(pars_path, mode='r', encoding='cp1251') as html_lines:
-        parser = Main_parser()
+        parser = ExtraDataParser()
         for line in html_lines:
             parser.feed(line.strip())
         start_date = datetime.datetime.strptime(
@@ -118,7 +89,7 @@ def wagon_data_extra_parser(pars_path):
     Возвращает словарь со списками необработанных аргументов для экземпляров класса Wagon()
     """
     with open(pars_path, mode='r', encoding='cp1251') as html_lines:
-        parser = Extra_parser()
+        parser = BaseDataParser()
         for line in html_lines:
             parser.feed(line.strip())
         raw_wagon_attrs = parser.wagon_attrs
